033bfadece6766064afc3471a4e196b5b24fbe3a
Rytis Kaplūnas <rytkaplunas@gmail.com>
2018-04-14 20:56:39 +0300

    Added entities

---
+++ DOL/Accounts/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOL.Accounts
+{
+    public class Customer : Account
+    {
+        public Card Card { get; set; }
+        public DeliveryAddress DeliveryAddress { get; set; }
+        //TODO: Add Order
+    }
+}
---
+++ DOL/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOL
+{
+    public class Card
+    {
+        public string Number { get; set; }
+        public string Holder { get; set; }
+        public int ExpYear { get; set; }
+        public int ExpMonth { get; set; }
+        public int CV { get; set; }
+    }
+}
