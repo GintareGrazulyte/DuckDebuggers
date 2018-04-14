bb3abb3e1ad4c070394ed3b4327d86a4eba41483
Tomas Monkevic <tomas.monkevic97@gmail.com>
2018-04-14 21:38:02 +0300

    Added login and register views. Added keys to entities

--- DOL/Accounts/Account.cs
+++ DOL/Accounts/Account.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace DOL.Accounts
 {
     public abstract class Account
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
---
+++ EShop/Controllers/CustomerController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DOL.Accounts;
+
+namespace EShop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Customer/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Customer customer)
+        {
+            return View(customer);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Customer customer)
+        {
+            var foundCustomer = new Object();
+            if(foundCustomer != null)
+            {
+                Session["UserId"] = foundCustomer;
+                return RedirectToAction("LoggedIn");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Wrong email or password");
+            }
+            return View(customer);
+        }
+
+        public ActionResult LoginIn(Object customer)
+        {
+            if(Session["UserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
