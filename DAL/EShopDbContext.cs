83c303c7f8072ac37ca01c2d6990cbbd582ebb28
Gražulytė Gintarė <gintare.grazulyte@mif.stud.vu.lt>
2018-04-14 18:15:34 +0300

    Layers

---
+++ DAL/EShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DOL.Objects;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace DAL.EShopDbContext
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext()
+            : base("DuckDebuggersEShop")
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
