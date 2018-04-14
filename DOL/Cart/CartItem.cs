16a418ece2e37a2c8eef30d5ddc95d748cbf861e
Edgaras <edgaras.miuler@gmail.com>
2018-04-14 20:35:34 +0300

    Added Cart & CartItem

---
+++ DOL/Cart/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using DOL.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOL.Cart
+{
+    public class CartItem
+    {
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+    }
+}
