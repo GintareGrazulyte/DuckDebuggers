1b1a7e0e98513d03eec0494e528920ec77000e20
Tomas Monkevic <tomas.monkevic97@gmail.com>
2018-04-19 19:29:35 +0300

    Fixed account id generation. Logout added

--- DOL/Accounts/Account.cs
+++ DOL/Accounts/Account.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DOL.Accounts
 {
     public abstract class Account
     {
         [Key]
-        public Guid Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         [Required]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter valid email.")]
         public string Email { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
