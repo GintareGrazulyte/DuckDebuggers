4f381ffd9e842761ecae57e3c50eabdc3d1a867c
Rytis Kaplūnas <rytkaplunas@gmail.com>
2018-04-22 22:01:59 +0300

    Simple category-item view

--- DOL/Category.cs
+++ DOL/Category.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DOL.Objects;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +11,10 @@ namespace DOL
 {
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Item> Items { get; set; }
     }
 }
--- DOL/Item.cs
+++ DOL/Item.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DOL.Objects
 {
     public class Item
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
