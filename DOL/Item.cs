5bd3bfc0c771d739badbf529e4465cdce6a361fc
Rytis Kaplūnas <rytkaplunas@gmail.com>
2018-04-22 20:59:07 +0300

    Added Category entity

--- DOL/Item.cs
+++ DOL/Item.cs
@@ -9,5 +9,6 @@ namespace DOL.Objects
         public string Name { get; set; }
         public int Price { get; set; }
         public string Description { get; set; }
+        public Category Category { get; set; }
     }
 }
