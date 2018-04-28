f620de90f67546d0596ec673c4d9895911ae27a6
Edgaras <edgaras.miuler@gmail.com>
2018-04-28 17:07:21 +0300

    the beginning of order history

---
+++ EShop/Models/OrderHistoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DOL.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EShop.Models
+{
+    public class OrderHistoryViewModel
+    {
+        public Order Order { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
