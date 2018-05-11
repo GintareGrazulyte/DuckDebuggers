5c919d138f8cc7570f9a814bd7012ba7402277b0
Tomas Monkevic <tomas.monkevic97@gmail.com>
2018-05-11 23:08:14 +0300

    Redirecting to custom error pages

---
+++ EShop/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace EShop.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult DefaultError()
+        {
+            return View();
+        }
+
+        public ActionResult NotFoundError()
+        {
+            return View();
+        }
+
+        public ActionResult InternalError()
+        {
+            return View();
+        }
+    }
+}
