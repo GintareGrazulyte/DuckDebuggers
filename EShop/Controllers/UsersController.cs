227c9206ea70e40fd8bbe2a9b0aa4b6559300e46
osvaldasj <jonaitis.osvaldas@gmail.com>
2018-05-16 22:33:00 +0300

    Implement search with sugestions for users

---
+++ EShop/Controllers/UsersController.cs
@@ -0,0 +1,93 @@
+﻿using BLL_API;
+using BOL.Accounts;
+using EShop.Attributes;
+using EShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EShop.Controllers
+{
+    [CustomAuthorization(LoginPage = "~/Admin/Login", Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly IAdminService _adminService;
+
+        public UsersController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public ActionResult Index()
+        {
+            var searchTerm = Request["search"];
+
+            List<UserViewModel.User> allCustomers = _adminService.GetCustomers()
+                .Select(x => new UserViewModel.User { Id = x.Id, Name = x.Name, Surname = x.Surname, Email = x.Email, IsActive = x.IsActive })
+                .Distinct().ToList();
+            List<UserViewModel.User> allAdmins = _adminService.GetAdmins()
+                .Select(x => new UserViewModel.User { Id = x.Id, Name = x.Name, Surname = x.Surname, Email = x.Email, IsActive = x.IsActive })
+                .Distinct().ToList();
+            List<UserViewModel.User> foundCustomers;
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                foundCustomers = allCustomers;
+            }
+            else
+            {
+                searchTerm = searchTerm.ToUpper();
+                foundCustomers = allCustomers.Where(x => x.Name.ToUpper().Contains(searchTerm) || x.Surname.ToUpper().Contains(searchTerm) || x.Email.ToUpper().Contains(searchTerm) || (x.Name.ToUpper() + " " + x.Surname.ToUpper()).Contains(searchTerm))
+                    .Select(x => new UserViewModel.User { Id = x.Id, Name = x.Name, Surname = x.Surname, Email = x.Email, IsActive = x.IsActive })
+                    .Distinct().ToList();
+            }
+
+            var view = new UserViewModel()
+            {
+                AllAdmins = allAdmins,
+                AllCUstomers = allCustomers,
+                FoundCustomers = foundCustomers
+            };
+            return View(view);
+        }
+
+        public ActionResult ChangeStatus(int id)
+        {
+            var account = _adminService.GetCustomer(id);
+            if (ModelState.IsValid)
+            {
+                if (account != null)
+                {
+                    _adminService.ChangeStatus(account);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _adminService.CreateAdmin(admin);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    ModelState.AddModelError("", ex.Message);
+                }
+            }
+            return View(admin);
+        }
+    }
+}
---
+++ EShop/Models/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Models
+{
+    public class UserViewModel
+    {
+        public List<User> FoundCustomers { get; set; }
+        public List<User> AllCUstomers { get; set; }
+        public List<User> AllAdmins { get; set; }
+
+        public class User
+        {
+            [Required]
+            public int Id { get; set; }
+            [Required]
+            [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter valid email.")]
+            public string Email { get; set; }
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            public string Surname { get; set; }
+            public bool IsActive { get; set; }
+        }
+    }
+}
