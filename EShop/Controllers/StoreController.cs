9fb5abda2a5c5d513e9e2ccaf8dafef0ac89212c
osvaldasj <jonaitis.osvaldas@gmail.com>
2018-05-17 11:08:02 +0300

    Implement search for products.
    Isue: can not display category through shop item.

--- EShop/Controllers/StoreController.cs
+++ EShop/Controllers/StoreController.cs
@@ -8,13 +8,14 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using EShop.Models;
 
 namespace EShop.Controllers
 {
     public class StoreController : Controller
     {
-        private ICategoryService _categoryService;
-        private IItemQueryService _itemQueryService;
+        private readonly ICategoryService _categoryService;
+        private readonly IItemQueryService _itemQueryService;
 
         public StoreController(ICategoryService categoryService, IItemQueryService itemQueryService)
         {
@@ -24,12 +25,28 @@ namespace EShop.Controllers
         // GET: Store
         public ActionResult Index()
         {
-            var categories = _categoryService.GetAllCategories();
+            var searchTerm = Request["Search"];
+            List<Item> allItems = _itemQueryService.GetAllItems().ToList();
+            List<Category> allCategories = _categoryService.GetAllCategories().ToList();
+            List<Item> items = null;
 
-            var items = (_itemQueryService.GetAllItems()).Where(i => i.CategoryId == null).ToList();
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                searchTerm = searchTerm.ToUpper();
+                items = allItems.Where(x => x.Name.ToUpper().Contains(searchTerm) || x.Description.ToUpper().Contains(searchTerm))
+                    .Distinct().ToList();
+            }
+
+            var categories = _categoryService.GetAllCategories();
             IEnumerable<Category> noCategory = new List<Category> { new Category { Id = 0, Items = items, Name = "No category" } };
             categories = categories.Concat(noCategory);
-            return View(categories);
+            var view = new StoreViewModel
+            {
+                AllItems = allItems,
+                DisplayedCategories = allCategories,
+                DisplayedItems = items
+            };
+            return View(view);
         }
         [ChildActionOnly]
         public ActionResult CategoryMenu()
---
+++ EShop/Models/StoreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BOL;
+using BOL.Objects;
+using System.Collections.Generic;
+
+namespace EShop.Models
+{
+    public class StoreViewModel
+    {
+        public List<Category> DisplayedCategories { get; set; }
+        public List<Item> DisplayedItems { get; set; }
+        public List<Item> AllItems { get; set; }
+    }
+}
