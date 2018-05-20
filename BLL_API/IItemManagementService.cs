aac7350e280f407ff09a17587154e6812431f910
Gražulytė Gintarė <gintare.grazulyte@mif.stud.vu.lt>
2018-05-20 16:12:43 +0300

    Ability for admin to choose if he wants to get an email with import / export log info

--- BLL_API/IItemManagementService.cs
+++ BLL_API/IItemManagementService.cs
@@ -8,8 +8,8 @@ namespace BLL_API
 {
     public interface IItemManagementService
     {
-        Task ImportItemsFromFile(Admin admin, string folderToFile, HttpPostedFileBase file, string imagesFolder);
-        Task ExportAllItemsToFile(Admin admin, IEnumerable<Item> allItems);
+        Task ImportItemsFromFile(Admin admin, string folderToFile, HttpPostedFileBase file, string imagesFolder, bool logInfoNeeded);
+        Task ExportAllItemsToFile(Admin admin, IEnumerable<Item> allItems, bool logInfoNeeded, string folderToSave);
         void CreateItemWithImage(Item itemToCreate, string folderToImage);
         void CreateItem(Item itemToCreate);
         void UpdateItem(Item itemToUpdate);
