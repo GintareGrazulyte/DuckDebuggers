f03760951484b10b6e641a150515dc0aecbbec20
Rytis Kaplūnas <rytkaplunas@gmail.com>
2018-05-31 01:34:28 +0300

    Discounts for items with no category

--- DOL/Discounts/AbsoluteDiscount.cs
+++ DOL/Discounts/AbsoluteDiscount.cs
@@ -12,10 +12,11 @@ namespace BOL.Discounts
 
         public override decimal CalculateDiscountedPrice(int itemPrice)
         {
-            if (itemPrice - AbsoluteValue < 0) throw new ArgumentException($"Negative price exception. " +
+            /*if (itemPrice - AbsoluteValue < 0) throw new ArgumentException($"Negative price exception. " +
                                                                         $"Can't apply absolute discount: {AbsoluteValue} " +
-                                                                        $"to item with price: {itemPrice}");
-            return itemPrice - AbsoluteValue;
+                                                                        $"to item with price: {itemPrice}");*/
+            var value = itemPrice - AbsoluteValue;
+            return value < 0 ? 0 : value;
         }
     }
 }
