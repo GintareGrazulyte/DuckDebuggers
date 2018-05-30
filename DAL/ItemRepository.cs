f9f513ff10150e206673f6f74eda5824997d3abf
eMiuler <edgaras.miuler@gmail.com>
2018-05-30 23:41:55 +0300

    fixed a bug where properties werent loaded and small html fixes

--- DAL/ItemRepository.cs
+++ DAL/ItemRepository.cs
@@ -37,13 +37,13 @@ namespace DAL
 
         public Item FindById(int? id)
         {
-            return DbContext.Items.Include("Category").Include("Discounts").Include("ItemProperties")
+            return DbContext.Items.Include("Category").Include("Discounts").Include("ItemProperties.Property")
                     .SingleOrDefault(c => c.Id == id);
         }
 
         public List<Item> GetAll()
         {
-            return DbContext.Items.Include("Discounts").Include("Category").Include("ItemProperties").ToList();
+            return DbContext.Items.Include("Discounts").Include("Category").Include("ItemProperties.Property").ToList();
         }
 
         public void Modify(Item item)
